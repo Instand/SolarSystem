6682d1a6370246b49962164ee34a79a81d7a3bbb
Arew <noobterr@yandex.ru>
2016-09-19 16:31:46 +0300

    Added visual object realization for 3d object logic programming

---
+++ Scene/visualsolarobject.h
@@ -0,0 +1,41 @@
+#ifndef VISUALSOLAROBJECT_H
+#define VISUALSOLAROBJECT_H
+
+#include <Qt3DLogic/QFrameAction>
+#include "Interface/ivisualsolarobject.h"
+
+namespace SolarSystem {
+
+    //class that represents logic programming for visual object on the scene
+    class VisualSolarObject : public IVisualSolarObject {
+
+        Q_OBJECT
+
+    public:
+        VisualSolarObject(Qt3DCore::QNode* parent = 0);
+
+    protected:
+        virtual void update(float deltaTime) = 0;
+
+    public:
+        virtual Qt3DCore::QTransform& transform() const = 0;
+        virtual Qt3DRender::QMesh& mesh() const = 0;
+
+        //realize some proterties
+        virtual void addLogic(LogicPtr func) override final;
+        virtual void clearLogic() override final;
+
+    private:
+        Logic logic;
+        Qt3DLogic::QFrameAction* _frameAction;
+
+    private slots:
+        void frameTick(float deltaTime);
+
+    signals:
+        void tick(float);
+    };
+
+}
+
+#endif // VISUALSOLAROBJECT_H
