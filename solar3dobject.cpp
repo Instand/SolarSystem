73deae77641e6b7960392663007014d9d4839482
Arew <noobterr@yandex.ru>
2016-09-19 16:59:14 +0300

    Solar3DObject changed concept

--- solar3dobject.cpp
+++ solar3dobject.cpp
@@ -1,36 +1,18 @@
 #include "solar3dobject.h"
 
 SolarSystem::Solar3dObject::Solar3dObject(Qt3DCore::QNode *parent) :
-    Qt3DCore::QEntity(parent),
-    _transform(new Qt3DCore::QTransform),
-    _mesh(new Qt3DRender::QMesh),
-    _frame(new Qt3DLogic::QFrameAction),
+    BaseVisualSolarObject(parent),
     _picker(new Qt3DRender::QObjectPicker),
     _material(new Qt3DExtras::QNormalDiffuseSpecularMapMaterial),
     _diffuseImage(new Qt3DRender::QTextureImage),
     _normalImage(new Qt3DRender::QTextureImage)
 {
 
-    _material.get()->diffuse()->addTextureImage(_diffuseImage.get());
-    _material.get()->normal()->addTextureImage(_normalImage.get());
+    _material->diffuse()->addTextureImage(_diffuseImage);
+    _material->normal()->addTextureImage(_normalImage);
 
-    addComponent(_transform.get());
-    addComponent(_mesh.get());
-    addComponent(_frame.get());
-    addComponent(_material.get());
-
-    //frame update
-    QObject::connect(_frame.get(), SIGNAL(triggered(float)), SLOT(frameTick(float)));
-}
-
-Qt3DCore::QTransform &SolarSystem::Solar3dObject::transform() const
-{
-    return *_transform;
-}
-
-Qt3DRender::QMesh &SolarSystem::Solar3dObject::mesh() const
-{
-    return *_mesh;
+    addComponent(_material);
+    addComponent(_picker);
 }
 
 Qt3DRender::QObjectPicker &SolarSystem::Solar3dObject::picker() const
@@ -53,23 +35,9 @@ Qt3DRender::QTextureImage &SolarSystem::Solar3dObject::normal() const
     return *_normalImage;
 }
 
-void SolarSystem::Solar3dObject::addLogic(SolarSystem::LogicPtr func)
+void SolarSystem::Solar3dObject::update(float deltaTime)
 {
-    additionalLogic.push_back(func);
-}
+    Q_UNUSED(deltaTime)
 
-void SolarSystem::Solar3dObject::clearLogic()
-{
-    additionalLogic.clear();
-}
-
-void SolarSystem::Solar3dObject::frameTick(float deltaTime)
-{
-    update(deltaTime);
-
-    //additional actions
-    for (auto elem : additionalLogic)
-        elem(deltaTime);
-
-    emit tick(deltaTime);
+    //mb we need something logic
 }
--- solar3dobject.h
+++ solar3dobject.h
@@ -1,25 +1,15 @@
 #ifndef SOLAR3DOBJECT_H
 #define SOLAR3DOBJECT_H
 
-#include <Qt3DCore/QEntity>
-#include <Qt3DCore/QTransform>
-#include <Qt3DRender/QMesh>
 #include <Qt3DExtras/QNormalDiffuseSpecularMapMaterial>
-#include <Qt3DRender/QTexture>
-#include <Qt3DLogic/QFrameAction>
 #include <Qt3DRender/QObjectPicker>
-#include <memory>
-#include <functional>
-#include <vector>
+#include <Qt3DRender/QTextureImage>
+#include "Scene/basevisualsolarobject.h"
 
 namespace SolarSystem {
 
-    //types
-    using LogicPtr = std::function<void(float)>;
-    using Logic = std::vector<LogicPtr>;
-
     //class for rendering base 3d object in solar system
-    class Solar3dObject : public Qt3DCore::QEntity {
+    class Solar3dObject : public BaseVisualSolarObject {
 
         Q_OBJECT
 
@@ -27,45 +17,22 @@ namespace SolarSystem {
         Solar3dObject(Qt3DCore::QNode* parent = 0);
 
         //get object properties
-        Qt3DCore::QTransform& transform() const;
-        Qt3DRender::QMesh& mesh() const;
         Qt3DRender::QObjectPicker& picker() const;
         Qt3DExtras::QNormalDiffuseSpecularMapMaterial& material() const;
         Qt3DRender::QTextureImage& diffuse() const;
         Qt3DRender::QTextureImage& normal() const;
 
-        //add logic
-        void addLogic(LogicPtr func);
-        void clearLogic();
-
     protected:
-
-        //frame tick for solar object logic programming
-        virtual void update(float deltaTime) = 0;
+        virtual void update(float deltaTime) override;
 
     private:
-
-        //transform + mesh
-        std::unique_ptr<Qt3DCore::QTransform> _transform;
-        std::unique_ptr<Qt3DRender::QMesh> _mesh;
-
-        //logic
-        std::unique_ptr<Qt3DLogic::QFrameAction> _frame;
-        std::unique_ptr<Qt3DRender::QObjectPicker> _picker;
+        Qt3DRender::QObjectPicker* _picker;
 
         //vizibility
-        std::unique_ptr<Qt3DExtras::QNormalDiffuseSpecularMapMaterial> _material;
-        std::unique_ptr<Qt3DRender::QTextureImage> _diffuseImage;
-        std::unique_ptr<Qt3DRender::QTextureImage> _normalImage;
+        Qt3DExtras::QNormalDiffuseSpecularMapMaterial* _material;
+        Qt3DRender::QTextureImage* _diffuseImage;
+        Qt3DRender::QTextureImage* _normalImage;
 
-        //additional logic
-        Logic additionalLogic;
-
-    private slots:
-        void frameTick(float deltaTime);
-
-    signals:
-        void tick(float);
     };
 
 }
