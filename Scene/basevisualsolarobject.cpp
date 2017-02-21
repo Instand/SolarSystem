e6e62afc2b14b9650a51380807eb7b3dcc76e5ef
Arew <noobterr@yandex.ru>
2017-02-21 11:47:44 +0300

    Setters/getters implementation added

--- Scene/basevisualsolarobject.cpp
+++ Scene/basevisualsolarobject.cpp
@@ -18,3 +18,73 @@ Qt3DRender::QMesh &SolarSystem::BaseVisualSolarObject::mesh() const
 {
     return *_mesh;
 }
+
+SolarSystem::SolarObjects SolarSystem::BaseVisualSolarObject::solarType() const
+{
+    return _solarType;
+}
+
+void SolarSystem::BaseVisualSolarObject::setSolarType(SolarSystem::SolarObjects type)
+{
+    _solarType = type;
+}
+
+float SolarSystem::BaseVisualSolarObject::tilt() const
+{
+    return _tilt;
+}
+
+void SolarSystem::BaseVisualSolarObject::setTilt(float tilt)
+{
+    _tilt = tilt;
+}
+
+float SolarSystem::BaseVisualSolarObject::roll() const
+{
+    return _roll;
+}
+
+void SolarSystem::BaseVisualSolarObject::setRoll(float roll)
+{
+    _roll = roll;
+}
+
+float SolarSystem::BaseVisualSolarObject::z() const
+{
+    return _z;
+}
+
+void SolarSystem::BaseVisualSolarObject::setZ(float z)
+{
+    _z = z;
+}
+
+float SolarSystem::BaseVisualSolarObject::y() const
+{
+    return _y;
+}
+
+void SolarSystem::BaseVisualSolarObject::setY(float y)
+{
+    _y = y;
+}
+
+float SolarSystem::BaseVisualSolarObject::x() const
+{
+    return _x;
+}
+
+void SolarSystem::BaseVisualSolarObject::setX(float x)
+{
+    _x = x;
+}
+
+float SolarSystem::BaseVisualSolarObject::r() const
+{
+    return _r;
+}
+
+void SolarSystem::BaseVisualSolarObject::setR(float r)
+{
+    _r = r;
+}
