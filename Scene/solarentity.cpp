4ebbced27daeaae1f87e0f7012a5459dbbd14ba3
Arew <noobterr@yandex.ru>
2017-04-07 11:38:58 +0300

    Create forward frame graph

--- Scene/solarentity.cpp
+++ Scene/solarentity.cpp
@@ -1,6 +1,6 @@
 #include "solarentity.h"
 #include <SolarCore/cameracontroller.h>
-#include <Qt3DRender/QFilterKey>
+#include <SolarCore/SolarRender/solarforwardframegraph.h>
 
 SolarSystem::SolarEntity::SolarEntity(QNode* parent):
     Qt3DCore::QEntity(parent),
@@ -26,44 +26,12 @@ SolarSystem::SolarEntity::SolarEntity(QNode* parent):
     controller->setCamera(mainCamera);
     controller->setLookSpeed(controller->lookSpeed() * 1.2f);
 
-    //render
-    filter = new Qt3DRender::QTechniqueFilter();
-
-    auto* key = new Qt3DRender::QFilterKey();
-    key->setName("renderingStyle");
-    key->setValue("forward");
-    filter->addMatch(key);
-
-    surfaceSelector = new Qt3DRender::QRenderSurfaceSelector(filter);
-
-    viewPort = new Qt3DRender::QViewport(surfaceSelector);
-    viewPort->setNormalizedRect(QRectF(0.0, 0.0, 1.0, 1.0));
-
-    cameraSelector = new Qt3DRender::QCameraSelector(viewPort);
-
-    clearBuffers = new Qt3DRender::QClearBuffers(cameraSelector);
-    clearBuffers->setBuffers(Qt3DRender::QClearBuffers::ColorDepthBuffer);
-    clearBuffers->setClearColor(QColor(Qt::black));
-
-    frustum = new Qt3DRender::QFrustumCulling(clearBuffers);
-
-    sortPolicy = new Qt3DRender::QSortPolicy(clearBuffers);
-
-    QVector<Qt3DRender::QSortPolicy::SortType> sortedVector;
-    sortedVector.push_back(Qt3DRender::QSortPolicy::StateChangeCost);
-    sortedVector.push_back(Qt3DRender::QSortPolicy::Material);
-    sortedVector.push_back(Qt3DRender::QSortPolicy::BackToFront);
-
-    sortPolicy->setSortTypes(sortedVector);
-
-    settings = new Qt3DRender::QRenderSettings();
-    settings->setActiveFrameGraph(filter);
+    auto* frameGraph = new SolarForwardFrameGraph(this);
+    frameGraph->setCamera(mainCamera);
 
     input = new Qt3DInput::QInputSettings();
 
-    cameraSelector->setCamera(mainCamera);
-
-    addComponent(settings);
+    addComponent(frameGraph);
     addComponent(input);
 
     //math core control
--- Scene/solarentity.h
+++ Scene/solarentity.h
@@ -5,15 +5,7 @@
 #include <Qt3DCore/QEntity>
 #include <solarsystemcore.h>
 #include <SolarCore/solaranimator.h>
-#include <Qt3DRender/QSortPolicy>
-#include <Qt3DRender/QClearBuffers>
-#include <Qt3DRender/QCameraSelector>
-#include <Qt3DRender/QViewport>
-#include <Qt3DRender/QRenderSurfaceSelector>
-#include <Qt3DRender/QTechniqueFilter>
-#include <Qt3DRender/QRenderSettings>
 #include <Qt3DInput/QInputSettings>
-#include <Qt3DRender/QFrustumCulling>
 #include <Qt3DLogic/QFrameAction>
 
 namespace SolarSystem {
@@ -23,8 +15,8 @@ namespace SolarSystem {
 namespace SolarSystem {
 
     //represents chain of solar objects (root entity)
-    class SolarEntity : public Qt3DCore::QEntity {
-
+    class SolarEntity : public Qt3DCore::QEntity
+    {
         Q_OBJECT
 
         Q_PROPERTY(SolarAnimator* animator READ animator)
@@ -60,15 +52,7 @@ namespace SolarSystem {
         //planets container
         PlanetsContainer* planetsContainer;
 
-        //render frame graph elements
-        Qt3DRender::QSortPolicy* sortPolicy;
-        Qt3DRender::QClearBuffers* clearBuffers;
-        Qt3DRender::QCameraSelector* cameraSelector;
-        Qt3DRender::QViewport* viewPort;
-        Qt3DRender::QRenderSurfaceSelector* surfaceSelector;
-        Qt3DRender::QTechniqueFilter* filter;
-        Qt3DRender::QFrustumCulling* frustum;
-        Qt3DRender::QRenderSettings* settings;
+        //input
         Qt3DInput::QInputSettings* input;
     };
 }
