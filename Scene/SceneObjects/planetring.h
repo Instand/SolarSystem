b50d5d9f8e8652deca4785053cdc7f4522fc6784
Arew <noobterr@yandex.ru>
2017-04-14 08:20:22 +0300

    Removed textures data, removed material, removed picker, solar3dobject as a base class

--- Scene/SceneObjects/planetring.h
+++ Scene/SceneObjects/planetring.h
@@ -1,44 +1,23 @@
 #ifndef PLANETRING_H
 #define PLANETRING_H
 
-#include <Scene/basevisualsolarobject.h>
-#include <Qt3DExtras/QNormalDiffuseMapAlphaMaterial>
-#include <Qt3DRender/QTextureImage>
-#include <Qt3DRender/QTextureWrapMode>
+#include <solar3dobject.h>
+#include <SolarCore/SolarRender/solarmaterial.h>
 
 namespace SolarSystem {
 
     //represents a planet ring, for example saturn and uranus rings
-    class PlanetRing : public BaseVisualSolarObject
+    class PlanetRing : public Solar3dObject
     {
         Q_OBJECT
 
     public:
         PlanetRing(Qt3DCore::QNode* parent = nullptr);
 
-        //get object properties
-        virtual Qt3DRender::QObjectPicker& picker() const override final;
-        Qt3DExtras::QNormalDiffuseMapAlphaMaterial& material() const;
-        Qt3DRender::QTextureImage& diffuse() const;
-
     protected:
 
         //main update
         virtual void update(float deltaTime) override;
-
-    protected:
-
-        //object picker
-        Qt3DRender::QObjectPicker* _picker;
-
-        //vizibility
-        Qt3DExtras::QNormalDiffuseMapAlphaMaterial* _material;
-
-        //main default texture
-        Qt3DRender::QTextureImage* _diffuse;
-
-        //texture setup
-        Qt3DRender::QTextureWrapMode wrapMode;
     };
 }
 
