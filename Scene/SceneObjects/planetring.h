43ea9953bdfc32a7fa8a1b15e1f6282d8138cc1c
Arew <noobterr@yandex.ru>
2017-09-25 09:51:26 +0300

    Fixes for two build types

--- Scene/SceneObjects/planetring.h
+++ Scene/SceneObjects/planetring.h
@@ -3,9 +3,14 @@
 
 #include <solar3dobject.h>
 #include <SolarCore/SolarRender/solarmaterial.h>
+#include <Scene/basevisualsolarobject.h>
+#include <Qt3DExtras/QNormalDiffuseMapAlphaMaterial>
+#include <Qt3DRender/QTextureImage>
+#include <Qt3DRender/QTextureWrapMode>
 
-namespace SolarSystem {
-
+namespace SolarSystem
+{
+#ifndef QT3D_MATERIALS
     //represents a planet ring, for example saturn and uranus rings
     class PlanetRing : public Solar3dObject
     {
@@ -19,6 +24,40 @@ namespace SolarSystem {
         //main update
         virtual void update(float deltaTime) override;
     };
+#else
+    //represents a planet ring, for example saturn and uranus rings
+    class PlanetRing : public BaseVisualSolarObject
+    {
+        Q_OBJECT
+
+    public:
+        PlanetRing(Qt3DCore::QNode* parent = nullptr);
+
+        //get object properties
+        virtual Qt3DRender::QObjectPicker& picker() const override final;
+        Qt3DExtras::QNormalDiffuseMapAlphaMaterial& material() const;
+        Qt3DRender::QTextureImage& diffuse() const;
+
+    protected:
+
+        //main update
+        virtual void update(float deltaTime) override;
+
+    protected:
+
+        //object picker
+        Qt3DRender::QObjectPicker* _picker;
+
+        //vizibility
+        Qt3DExtras::QNormalDiffuseMapAlphaMaterial* _material;
+
+        //main default texture
+        Qt3DRender::QTextureImage* _diffuse;
+
+        //texture setup
+        Qt3DRender::QTextureWrapMode wrapMode;
+    };
+#endif
 }
 
 #endif // PLANETRING_H
