430c5bdf7da0a494d6ec81c4ae5f3ba70f53789e
Arew <noobterr@yandex.ru>
2017-10-04 10:53:48 +0300

    Refactoring

--- Scene/SceneObjects/emptysolarobject.h
+++ Scene/SceneObjects/emptysolarobject.h
@@ -3,15 +3,15 @@
 
 #include "solar3dobject.h"
 
-namespace SolarSystem {
-
+namespace SolarSystem
+{
     //empty object can be placed on the scene without any rendering, but with some interesting features
-    class EmptySolarObject: public Solar3dObject {
-
+    class EmptySolarObject: public Solar3dObject
+    {
         Q_OBJECT
 
     public:
-        EmptySolarObject(Qt3DCore::QNode* parent = 0);
+        EmptySolarObject(Qt3DCore::QNode* parent = nullptr);
 
     protected:
         virtual void update(float deltaTime) override;
