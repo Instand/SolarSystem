88d3084db8d2edd57d5ceca6d4632e835eae1017
Arew <noobterr@yandex.ru>
2017-10-11 16:23:02 +0300

    Diffuse material 3d object added

---
+++ Scene/MaterialObjects/diffuseobject.cpp
@@ -0,0 +1,30 @@
+#include "diffuseobject.h"
+#include <QTextureWrapMode>
+#include <QAbstractTexture>
+
+SolarSystem::DiffuseObject::DiffuseObject(Qt3DCore::QNode* parent):
+    Solar3dObject(parent)
+{
+    _materialType = SolarMaterials::Diffuse;
+    _material = new Qt3DExtras::QDiffuseMapMaterial();
+
+    auto mat = qobject_cast<Qt3DExtras::QDiffuseMapMaterial*>(_material);
+    Qt3DRender::QTextureWrapMode wrapMode;
+
+    wrapMode.setX(Qt3DRender::QTextureWrapMode::WrapMode::Repeat);
+    wrapMode.setY(Qt3DRender::QTextureWrapMode::WrapMode::Repeat);
+    wrapMode.setZ(Qt3DRender::QTextureWrapMode::WrapMode::Repeat);
+
+    mat->diffuse()->setWrapMode(wrapMode);
+    mat->diffuse()->setGenerateMipMaps(true);
+    mat->diffuse()->setMagnificationFilter(Qt3DRender::QAbstractTexture::Linear);
+    mat->diffuse()->setMinificationFilter(Qt3DRender::QAbstractTexture::LinearMipMapLinear);
+    mat->diffuse()->setMaximumAnisotropy(16.0f);
+
+    mat->setShininess(10000000);
+    mat->setSpecular(QColor(qRgba(0,0,0,255)));
+    mat->setAmbient(QColor(qRgba(10,10,10,255)));
+    mat->setTextureScale(1.0f);
+
+    addComponent(_material);
+}
