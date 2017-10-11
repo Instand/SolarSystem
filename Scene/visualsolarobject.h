a9a5e6eb4b41be4c064f0513e0c5bfd36d29204d
Arew <noobterr@yandex.ru>
2017-10-11 16:19:34 +0300

    Material + material type

--- Scene/visualsolarobject.h
+++ Scene/visualsolarobject.h
@@ -18,11 +18,13 @@ namespace SolarSystem
         virtual void update(float deltaTime) = 0;
 
     public:
-        virtual Qt3DCore::QTransform& transform() const = 0;
-        virtual Qt3DRender::QMesh& mesh() const = 0;
-        virtual Qt3DRender::QObjectPicker& picker() const = 0;
+        virtual Qt3DCore::QTransform* transform() const = 0;
+        virtual Qt3DRender::QMesh* mesh() const = 0;
+        virtual Qt3DRender::QObjectPicker* picker() const = 0;
         virtual SolarObjects solarType() const = 0;
         virtual void setSolarType(SolarObjects type) = 0;
+        virtual SolarMaterials materialType() const = 0;
+        virtual Qt3DRender::QMaterial* material() const = 0;
         virtual double r() const = 0;
         virtual void setR(double r) = 0;
         virtual double x() const = 0;
