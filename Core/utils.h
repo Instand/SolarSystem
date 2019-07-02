afb19ccc011bb2723f2ef9b871cf6efac75b0ac5
Arew <noobterr@yandex.ru>
2019-07-02 17:06:15 +0300

    Add utils lerp and slerp methods

--- Core/utils.h
+++ Core/utils.h
@@ -1,6 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <algorithm>
+#include <QVector3D>
+
 namespace SolarSystem
 {
     // Additional class with solar system common helper methods
@@ -21,6 +24,24 @@ namespace SolarSystem
         static float calculateTimeScale(int year, int month, int day)
         {
             return 367 * year - 7 * (year + (month + 9) / 12) / 4 + 275 * month / 9 + day - 730530;
+        }
+
+        // linear interpolation, time range [0, 1]
+        static QVector3D lerp(const QVector3D& start, const QVector3D& end, float time)
+        {
+            return (start + (end - start) * time);
+        }
+
+        // spherical linear interpolation from https://en.wikipedia.org/wiki/Slerp
+        static QVector3D slerp(const QVector3D& start, const QVector3D& end, float time)
+        {
+            auto dot = QVector3D::dotProduct(start, end);
+            auto dotValue = std::clamp(dot, -1.0f, 1.0f);
+
+            auto theta = std::acos(dotValue) * time;
+            auto relativeVector = (end - start * dotValue).normalized();
+
+            return (start * std::cos(theta)) + (relativeVector * std::sin(theta));
         }
     };
 }
